cedf48993acd2684859c91ead5d20afc36330e02
Ines Clavel <inesclavel@gmail.com>
2022-05-02 12:22:29 +0100

    Area method implementation

--- src/main/java/Circle.java
+++ src/main/java/Circle.java
@@ -1,2 +1,12 @@
-public class Circle {
+import java.awt.*;
+
+public class Circle  extends Figure{
+    public Circle(Color color) {
+        super(color);
+    }
+
+    @Override
+    public double area() {
+        return 0;
+    }
 }
--- src/main/java/Figure.java
+++ src/main/java/Figure.java
@@ -3,9 +3,14 @@ import java.awt.*;
 public abstract class Figure {
 
     private Color color;
+    private double area;
 
     public Figure(Color color) {
         this.color = color;
     }
+
+    public abstract double area ();
+
+
 }
 
